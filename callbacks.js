8b2ad5a74db38f73057fbe6f9ecf8ee8b6fdd924
Lizza Reyes <Lizzareyes7@gmail.com>
2017-09-19 19:19:06 -0400

    Update callbacks.js

--- callbacks.js
+++ callbacks.js
@@ -5,3 +5,10 @@ function addTwo(num){
 function addS(thing) {
   return thing + 's';
 }
+
+function map(numList, callback){
+  var numList = [5, 4, 3, 2, 1];
+  var callback = numList.map(function(callback) {
+    return numList*2; 
+  } );  
+};
