803b115c34e1e87c7013a175a0fc709c97d5768f
Eric <dphillips@fvi.edu>
2017-09-19 19:45:19 -0400

    Update callbacks.js

--- callbacks.js
+++ callbacks.js
@@ -1,11 +1,14 @@
+//challenge 1
 function addTwo(num){
   return num + 2;
 }
 
+//challenge 2
 function addS(thing) {
   return thing + 's';
 }
 
+//challenge 3
 function map(numList, callback){
   var numList = [5, 4, 3, 2, 1];
   var callback = numList.map(function(callback) {
@@ -13,8 +16,16 @@ function map(numList, callback){
   } );  
 };
 
+//challenge 4
 var alphabet = '';
 var letters = ['a', 'b', 'c', 'd'];
 letters.forEach(function(alphabet) {
     console.log(alphabet);
 });
+
+//extension 2
+var nums = [4,1,3]
+nums.reduce(function(sum, value) {
+  return sum + value;
+}, 0);
+
