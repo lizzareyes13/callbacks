50b09d3f71bc1866120940a94c4f72440ee2d6f8
Lizza Reyes <Lizzareyes7@gmail.com>
2017-09-20 22:11:23 -0400

    Update callbacks.js

--- callbacks.js
+++ callbacks.js
@@ -45,8 +45,37 @@ mapWith(array, kalbak);
 
 
 //extension 2
-var nums = [4,1,3]
-nums.reduce(function(sum, value) {
-  return sum + value;
-}, 0);
+// var nums = [4,1,3]
+// nums.reduce(function(sum, value) {
+//   return sum + value;
+// }, 0);
 
+function reduce(array, callback, initialValue = 0){
+	let accumulate = initialValue;
+	
+	for(let i = 0; i < accumulator.length; i++){
+		accumulate = callback(accumulate, array[i]);
+	}
+	return accumulate;
+}
+const multIt = (a,b) => a * b;
+const nums = [4,1,3];
+
+console.log(reduce(nums, multIt, 1));
+
+
+
+//extension 3
+function intersection(a,b){
+	var array = [];
+	
+	for (let i = 0; i < a.length; i++){
+		for(let j = 0; j < b.length; j++){
+			if(a[i] === b[j]){
+				array.push(a[i]);
+			}
+		}
+	}
+	return array;
+}
+console.log(intersection([5,6,7,8],[1,2,3,4]));
